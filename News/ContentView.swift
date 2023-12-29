dea09c4798f7b0fa2695e7c188c57c834e023fc1
salomege <salomegelenidze@gmail.com>
2023-12-29 12:36:34 -0800

    make edditions

--- News/ContentView.swift
+++ News/ContentView.swift
@@ -14,28 +14,28 @@ struct ContentView: View {
     @StateObject private var viewModel = ContentViewModel()
     @State private var selectedFontSize: CGFloat = 16
     
-    //MARK: - Body
+    // MARK: - Body
+    
     var body: some View {
         NavigationStack {
             if viewModel.isLoading {
-                
                 ProgressView()
+                    .accessibility(hidden: true)
             } else {
-                
                 List(0..<viewModel.articles.count, id: \.self) { index in
                     Text(viewModel.articles[index].title)
                         .font(.system(size: selectedFontSize))
+                        .accessibility(label: Text("Article Title"))
                 }
                 .toolbar {
-                    
-                    ToolbarItem( placement: .navigationBarLeading) {
+                    ToolbarItem(placement: .navigationBarTrailing) {
                         Menu {
-                            Button("Small") {selectedFontSize = 12}
-                            Button("Medium") {selectedFontSize = 16}
-                            Button("Large") {selectedFontSize = 18}
-                            Button("Extra Large") {selectedFontSize = 22}
+                            Button("Small") { selectedFontSize = 12 }
+                            Button("Medium") { selectedFontSize = 16 }
+                            Button("Large") { selectedFontSize = 18 }
                         } label: {
                             Label("Font Size", systemImage: "textformat.size")
+                                .accessibility(label: Text("Font Size Menu"))
                         }
                     }
                 }
